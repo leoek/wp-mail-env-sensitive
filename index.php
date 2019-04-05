67978051e259ab6a7b9417043669a4cb3641ff02
Martin Rehberger <martin@matchwerk.de>
2019-04-05 17:59:48 +0200

    WP coding standards and minor cosmetics

--- index.php
+++ index.php
@@ -3,44 +3,46 @@
 Plugin Name: wp-mail-env-sensitive
 Version: 1.1.0
 Author: Leonard Krause, Matchwerk Solutions GmbH
+Author URI: https://matchwerk.de
 Description: This Plugin reads the env vars MTA_HOSTNAME, WP_MAIL_FROMNAME and WP_MAIL_FROMNAME and updates the wordpress mail sender accordingly.
 */
 
-define("ENV_MTA_HOSTNAME", 'MTA_HOSTNAME');
-define("ENV_WP_MAIL_FROM", 'WP_MAIL_FROM');
-define("ENV_WP_MAIL_FROMNAME", 'WP_MAIL_FROMNAME');
+define('ENV_MTA_HOSTNAME', 'MTA_HOSTNAME');
+define('ENV_WP_MAIL_FROM', 'WP_MAIL_FROM');
+define('ENV_WP_MAIL_FROMNAME', 'WP_MAIL_FROMNAME');
 
-function WPMailDockerEnv_from( $originalFrom ) {
+function WPMailDockerEnv_from($originalFrom)
+{
     $from = defined(ENV_WP_MAIL_FROM) ? WP_MAIL_FROM : getenv(ENV_WP_MAIL_FROM);
-    if ($from){
+    if ($from) {
         return $from;
     }
     if (defined('FILTER_VALIDATE_EMAIL') && filter_var($originalFrom, FILTER_VALIDATE_EMAIL)) {
         return $originalFrom;
     }
     $mtaHostname = defined(ENV_MTA_HOSTNAME) ? MTA_HOSTNAME : getenv(ENV_MTA_HOSTNAME);
-    if ($mtaHostname){
-        return "wordpress@".$mtaHostname;
+    if ($mtaHostname) {
+        return 'wordpress@'.$mtaHostname;
     }
-    return "doNotReply@wordpress";
+    return 'doNotReply@wordpress';
 }
  
-function WPMailDockerEnv_fromName( $originalFromName ) {
+function WPMailDockerEnv_fromName($originalFromName)
+{
     $fromName = defined(ENV_WP_MAIL_FROMNAME) ? WP_MAIL_FROMNAME : getenv(ENV_WP_MAIL_FROMNAME);
-    if ($fromName){
+    if ($fromName) {
         return $fromName;
     }
     return $originalFromName;
 }
 
-add_filter( 'wp_mail_from', 'WPMailDockerEnv_from' );
-add_filter( 'wp_mail_from_name', 'WPMailDockerEnv_fromName' );
+add_filter('wp_mail_from', 'WPMailDockerEnv_from', 99999);
+add_filter('wp_mail_from_name', 'WPMailDockerEnv_fromName', 99999);
 
 /**
  * Log mail errors if they happen!
  */
-add_action('wp_mail_failed', function($e) {
-    error_log( print_r($e, true) );
+add_action('wp_mail_failed', function ($e) {
+    error_log(print_r($e, true));
 });
 
-?>
